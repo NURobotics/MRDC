347e911960afafd325a428ae1da374d03782f1d8
Andrew SaLoutos <andrewsaloutos2018@u.northwestern.edu>
2017-09-03 16:51:39 -0500

    testing new comms code

--- RPi for Demo/Nano_code/comms_test/comms_test.ino
+++ RPi for Demo/Nano_code/comms_test/comms_test.ino
@@ -1,36 +1,3 @@
-#include <Servo.h>
-
-// Arduino nano taking control signals from a Raspberry Pi
-
-// Motor pins ***********************************************************
-
-// Drive
-// Motor 1
-const int en1 = 3;
-const int ph1 = 2;
-
-// Motor 2
-const int en2 = 5;
-const int ph2 = 4;
-
-// Motor 3
-const int en3 = 6;
-const int ph3 = 7;
-
-// Motor 4
-const int en4 = 9;
-const int ph4 = 8;
-
-// Other
-// Hopper
-const int en5 = 11;
-const int ph5 = 12;
-
-// Servo
-const int wall_pwm = 10;
-Servo wall;
-bool wall_state;
-
 const byte numChars = 32;
 char receivedChars[numChars];
 char tempChars[numChars];        // temporary array for use when parsing
@@ -43,28 +10,8 @@ signed int newSpeeds[4];
 boolean newData = false;
 
 void setup() {
-  pinMode(en1, OUTPUT);
-  pinMode(ph1, OUTPUT);
-
-  pinMode(en2, OUTPUT);
-  pinMode(ph2, OUTPUT);
-
-  pinMode(en3, OUTPUT);
-  pinMode(ph3, OUTPUT);
-
-  pinMode(en4, OUTPUT);
-  pinMode(ph4, OUTPUT);
-
-  pinMode(en5, OUTPUT);
-  pinMode(ph5, OUTPUT);
-
-  pinMode(wall_pwm, OUTPUT);
-
-  wall.attach(wall_pwm, 900, 2100);
-  wall.write(0); // set servo to initial position
-  wall_state = 1; // set state to 1
-
-  Serial.begin(115200);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
   Serial.println("Format: <A,0,0,0,0>");
 }
 
@@ -78,35 +25,20 @@ void loop() {
             //   because strtok() used in parseData() replaces the commas with \0
         parseData();
         
-        // change motor speeds based on command_type
+        // change motor speeds here
         if (command_type == 'D'){
-          
           Serial.println(newSpeeds[0]);
           Serial.println(newSpeeds[1]);
           Serial.println(newSpeeds[2]);
           Serial.println(newSpeeds[3]);
-
-          move_motor1(newSpeeds[0]);
-          move_motor2(newSpeeds[1]);
-          move_motor3(newSpeeds[2]);
-          move_motor4(newSpeeds[3]);
-                    
         } else if (command_type == 'H') {
-          
           Serial.println(newSpeeds[0]);
-
-          move_hopper(newSpeeds[0]);
-          
         } else if (command_type == 'W') {
-          
           Serial.println("wall");
-
-          release_wall();
-          
         }
+//        showParsedData();
 
         newData = false;
-        
     }
 }
 
@@ -177,63 +109,3 @@ void showParsedData() {
     Serial.println(newSpeeds[3]);
 }
 
-
-void move_motor1(int spd) {
-  if (spd>0) {
-    analogWrite(en1, spd);
-    digitalWrite(ph1, HIGH);
-  } else {
-    analogWrite(en1, -spd);
-    digitalWrite(ph1, LOW);
-  }
-}
-
-void move_motor2(int spd) {
-  if (spd>0) {
-    analogWrite(en2, spd);
-    digitalWrite(ph2, HIGH);
-  } else {
-    analogWrite(en2, -spd);
-    digitalWrite(ph2, LOW);
-  }
-}
-
-void move_motor3(int spd) {
-  if (spd>0) {
-    analogWrite(en3, spd);
-    digitalWrite(ph3, HIGH);
-  } else {
-    analogWrite(en3, -spd);
-    digitalWrite(ph3, LOW);
-  }
-}
-
-void move_motor4(int spd) {
-  if (spd>0) {
-    analogWrite(en4, spd);
-    digitalWrite(ph4, HIGH);
-  } else {
-    analogWrite(en4, -spd);
-    digitalWrite(ph4, LOW);
-  }
-}
-
-void move_hopper(int spd) {
-  if (spd>0) {
-    analogWrite(en5, spd);
-    digitalWrite(ph5, HIGH);
-  } else {
-    analogWrite(en5, -spd);
-    digitalWrite(ph5, LOW);
-  }
-}
-
-void release_wall() {
-  if(wall_state == 1) { //wall is up, pull it down
-    wall.write(180);
-    wall_state = 0;
-  } else {
-    wall.write(0);
-    wall_state = 1;
-  }
-}
